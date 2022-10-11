508166066eb86ad884559513efc49252adf14d09
Joel <joel960801@gmail.com>
2022-10-11 07:41:03 +0200

    added useParams

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
 
 const QuoteDetail = () => {
+  const params = useParams();
   return (
-    <div>QuoteDetail</div>
-  )
-}
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
+};
 
-export default QuoteDetail
+export default QuoteDetail;
