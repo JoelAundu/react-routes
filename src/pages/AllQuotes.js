87fa93550b000c8c3488c8806227b173c6b4c489
Joel <joel960801@gmail.com>
2022-10-19 20:34:25 +0200

    created second data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const DUMMY_QUOTES = [
   {
-    id:"q1",
+    {id:"q1",
     author:"Max",
-    text:"Learning React is fun"
-
-  }
+    text:"Learning React is fun"},
+    {id:"q2",
+    author:"Juice",
+    text:"Learning React is great"}
+  },
 ];
 const AllQuotes = () => {
   return <div>AllQuotes</div>;
